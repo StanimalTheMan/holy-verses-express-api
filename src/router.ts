64a63303a090c4de3792b0b1800f5a8a2ed96453
StanimalTheMan <sjc591@nyu.edu>
2023-02-18 09:10:43 -0500

    include app router, exp. with middleware

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,12 @@ import { Router } from 'express'
 
 const router = Router()
 
-router.get('/verse', () => {})
+router.get('/verse', (req, res) => {
+    res.json({message: 'message'})
+})
 router.get('/verse/:id', () => {})
 router.put('/verse/:id', () => {})
 router.post('/verse', () => {})
 router.delete('/verse/:id', () => {})
+
+export default router
