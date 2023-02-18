d0f90cf1d8d6ede7194054d9acf0ca322fc5fe09
StanimalTheMan <sjc591@nyu.edu>
2023-02-17 23:41:03 -0500

    Define skeleton for routes to handle verse data handling

---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.get('/verse', () => {})
+router.get('/verse/:id', () => {})
+router.put('/verse/:id', () => {})
+router.post('/verse', () => {})
+router.delete('/verse/:id', () => {})
