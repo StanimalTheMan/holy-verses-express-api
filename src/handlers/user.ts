4a9d8dcafeb8b3e018acfc145ccba3a07709cc10
StanimalTheMan <sjc591@nyu.edu>
2023-02-26 21:01:00 -0500

    Working on adding validation to routes, add a theme of dark when creating user?

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -5,7 +5,8 @@ export const createNewUser = async (req, res) => {
   const user = await prisma.user.create({
     data: {
       username: req.body.username,
-      password: await hashPassword(req.body.password)
+      password: await hashPassword(req.body.password),
+      theme: 'Dark'
     }
   })
 
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { body, validationResult } from "express-validator";
 
 const router = Router()
 
@@ -6,7 +7,14 @@ router.get('/verse', (req, res) => {
   res.json({message: 'message'})
 })
 router.get('/verse/:id', () => {})
-router.put('/verse/:id', () => {})
+router.put('/verse/:id', body('content').isString(), (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors)
+    if (!errors.isEmpty()) {
+        res.status(400);
+        res.json({ errors: errors.array() });
+    }
+})
 router.post('/verse', () => {})
 router.delete('/verse/:id', () => {})
 
