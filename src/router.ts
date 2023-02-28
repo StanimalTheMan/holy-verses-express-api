241645cb4089e98f012b50b610ffd2c0259a737c
StanimalTheMan <sjc591@nyu.edu>
2023-02-27 22:56:15 -0500

    try to reuse validation error logic & start to validate some of fields in put verse request

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,25 @@
-import { Router } from 'express'
-import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
+import { Router } from "express";
+import { body, check, validationResult } from "express-validator";
 
-const router = Router()
+const router = Router();
 
-router.get('/verse', (req, res) => {
-  res.json({message: 'message'})
-})
-router.get('/verse/:id', () => {})
-router.put('/verse/:id', body('content').isString(), (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors)
-    if (!errors.isEmpty()) {
-        res.status(400);
-        res.json({ errors: errors.array() });
-    }
-})
-router.post('/verse', () => {})
-router.delete('/verse/:id', () => {})
+router.get("/verse", (req, res) => {
+  res.json({ message: "message" });
+});
+router.get("/verse/:id", () => {});
+router.put(
+  "/verse/:id",
+  body("content").isString(),
+  body("book").isString(),
+  body("chapter").isInt(),
+  body("translation").isString(),
+  body("verses").isArray({ min: 1 }),
+  check("verses.*").isInt(),
+  handleInputErrors,
+  (req, res) => {}
+);
+router.post("/verse", () => {});
+router.delete("/verse/:id", () => {});
 
-export default router
+export default router;
