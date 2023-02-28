76a34b0e6998d540ab6ce83f8dc78682d2162a95
StanimalTheMan <sjc591@nyu.edu>
2023-02-28 13:52:43 -0500

    try to add unique index constraint to id and belongstoid for updating and deleting verse

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,34 +1,35 @@
-import { hashPassword, createJWT, comparePasswords } from './../modules/auth';
-import prisma from '../db'
+import { hashPassword, createJWT, comparePasswords } from "./../modules/auth";
+import prisma from "../db";
 
 export const createNewUser = async (req, res) => {
   const user = await prisma.user.create({
     data: {
       username: req.body.username,
       password: await hashPassword(req.body.password),
-      theme: 'Dark'
-    }
-  })
+      theme: "Dark",
+    },
+  });
 
-  const token = createJWT(user)
-  res.json({token})
-}
+  const token = createJWT(user);
+  res.json({ token });
+};
 
 export const signin = async (req, res) => {
   const user = await prisma.user.findUnique({
     where: {
-      username: req.body.username
-    }
-  })
+      username: req.body.username,
+    },
+  });
+  console.log(user);
 
-  const isValid = await comparePasswords(req.body.password, user.password)
+  const isValid = await comparePasswords(req.body.password, user.password);
 
   if (!isValid) {
-    res.status(401)
-    res.json({message: 'Invalid username or password'})
-    return
+    res.status(401);
+    res.json({ message: "Invalid username or password" });
+    return;
   }
 
-  const token = createJWT(user)
-  res.json({ token })
-}
+  const token = createJWT(user);
+  res.json({ token });
+};
--- src/handlers/verse.ts
+++ src/handlers/verse.ts
@@ -27,3 +27,63 @@ export const getOneVerse = async (req, res) => {
 
   res.json({ data: verse });
 };
+
+export const createVerse = async (req, res) => {
+  const verse = await prisma.verse.create({
+    data: {
+      content: req.body.content,
+      book: req.body.book,
+      chapter: req.body.chapter,
+      translation: req.body.translation,
+      verses: req.body.verses,
+      belongsToId: req.user.id,
+      updatedAt: new Date().toISOString(),
+    },
+  });
+
+  res.json({ data: verse });
+};
+
+export const updateVerse = async (req, res) => {
+  const updatedData = {};
+  // probs a better way to optionally add to data
+  if (req.body.content) {
+    updatedData["content"] = req.body.content;
+  }
+  if (req.body.book) {
+    updatedData["book"] = req.body.book;
+  }
+  if (req.body.chapter) {
+    updatedData["chapter"] = req.body.chapter;
+  }
+  if (req.body.translation) {
+    updatedData["translation"] = req.body.translation;
+  }
+  if (req.body.verses) {
+    updatedData["verses"] = req.body.verses;
+  }
+  const updated = await prisma.verse.update({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+    data: updatedData,
+  });
+
+  res.json({ data: updated });
+};
+
+export const deleteVerse = async (req, res) => {
+  const deleted = await prisma.verse.delete({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+  });
+
+  res.json({ data: deleted });
+};
